91fb17b54a3f14ba1cae357c4fe6294201c9ba5a
dennismuturia <dmuturia2922@gmail.com>
2017-07-26 15:31:17 +0300

    Added the update functionality

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateProductForm: false,
+  actions:{
+    update(product, params) {
+      this.sendAction('update', product, params);
+    }
+  }
 });
