5317629568c89cb9d98d805f5249427231ce9e37
charliekaks <kaksunchained@gmail.com>
2017-07-27 10:53:01 +0300

    made to changes goods.hbs

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortedReviews: Ember.computed.sort('model', 'sortDefinition'),
-  sortDefinition: ['price:desc'],
   updateProductForm: false,
   actions:{
     update(product, params) {
