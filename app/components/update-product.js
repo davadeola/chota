30253386c8e8768bc434a8c1fd951a461f61d711
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-07-28 11:14:01 +0300

    fixed an error on update button

--- app/components/update-product.js
+++ app/components/update-product.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   updateProductForm: false,
 actions:{
-    update(product) {
+  updateProductForm(){
+    this.set('updateProductForm', true);
+  },
+    update(product){
       var params = {
         name: this.get('name'),
         price: this.get('price'),
@@ -11,7 +14,7 @@ actions:{
         quantity: this.get('quantity'),
         img: this.get('img')
       };
-
+      this.set('updateProductForm', false);
       this.sendAction('update', product, params);
     }
   }
